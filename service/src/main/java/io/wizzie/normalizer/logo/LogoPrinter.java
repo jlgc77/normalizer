55acea4bd9d0d5123a6a065ef1a31d68be802567
Pablo Casares <pcasares@wizzie.io>
2018-12-14 11:09:50 +0100

    Remove builder ref from LogoPrinter

--- service/src/main/java/io/wizzie/normalizer/logo/LogoPrinter.java
+++ service/src/main/java/io/wizzie/normalizer/logo/LogoPrinter.java
@@ -1,11 +1,10 @@
 package io.wizzie.normalizer.logo;
 
-import io.wizzie.normalizer.builder.Builder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogoPrinter {
-    private static final Logger log = LoggerFactory.getLogger(Builder.class);
+    private static final Logger log = LoggerFactory.getLogger(LogoPrinter.class);
 
     public static void PrintLogo() {
         log.info("\n" +
